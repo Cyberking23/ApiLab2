1e5a3bbb54a1d08dd5fcfc9f393f12638d732f63
Carlos Eduardo Merino Ventura <102163019+Cyberking23@users.noreply.github.com>
2025-09-16 11:51:15 -0600

    Api Inicializada
    Api Inicializada

---
+++ src/main/java/org/example/apilab2/ApiLab2Application.java
@@ -0,0 +1,13 @@
+package org.example.apilab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/apilab2/ApiLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apilab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
