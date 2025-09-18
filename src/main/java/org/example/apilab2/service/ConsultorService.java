5f2716277ac87a3bb39a65a93ba785de39c10323
Carlos Eduardo Merino Ventura <102163019+Cyberking23@users.noreply.github.com>
2025-09-18 09:47:11 -0600

    Update ConsultorService
    Use case for creating consultants.

--- src/main/java/org/example/apilab2/service/ConsultorService.java
+++ src/main/java/org/example/apilab2/service/ConsultorService.java
@@ -1,4 +1,5 @@
 package org.example.apilab2.service;
-
-public class ConsultorService {
+import org.example.apilab2.repository.domain.Consultor;
+public interface ConsultorService {
+    Consultor crear(String nombre, String especializacion, Boolean inActivoActualizado);
 }
