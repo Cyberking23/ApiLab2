ff9959dd40f1ec2f3a19a9c4c156d8d65725796e
Carlos Eduardo Merino Ventura <102163019+Cyberking23@users.noreply.github.com>
2025-09-18 09:05:06 -0600

    ApiError Wrapper
    allows returning multiple validation errors

--- src/main/java/org/example/apilab2/configuration/web/ApiErrorWrapper.java
+++ src/main/java/org/example/apilab2/configuration/web/ApiErrorWrapper.java
@@ -1,4 +1,17 @@
 package org.example.apilab2.configuration.web;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class ApiErrorWrapper {
+    private final List<ApiError> errors = new ArrayList<>();
+    public void addApiError(ApiError error) { errors.add(error); }
+    public void addFieldError(String type, String title, String source, String description) {
+        errors.add(ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .type(type).title(title).source(source).description(description).build());
+    }
 }
