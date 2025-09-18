10e7ba0f72df7664a6a01343b4bab5f8d66b805d
Carlos Eduardo Merino Ventura <102163019+Cyberking23@users.noreply.github.com>
2025-09-18 09:33:13 -0600

    Update Participante
    Person who signs up for programs; unique email address for monitoring purposes.

--- src/main/java/org/example/apilab2/repository/domain/Participante.java
+++ src/main/java/org/example/apilab2/repository/domain/Participante.java
@@ -1,4 +1,21 @@
 package org.example.apilab2.repository.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Set;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+@Entity @Table(name = "participantes")
 public class Participante {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false) private String nombre;
+    @Column(nullable=false, unique=true) private String email;
+    @Column(nullable=false) private String genero;
+    private String nivelEducativo;
+    private Boolean ingresoFormal;
+
+    @ManyToMany(mappedBy = "participantes")
+    private Set<Programa> programas;
 }
