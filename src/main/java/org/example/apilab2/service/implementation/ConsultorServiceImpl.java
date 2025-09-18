23fe822ea223d0b4249eff5ab8890ef1c91b1691
Carlos Eduardo Merino Ventura <102163019+Cyberking23@users.noreply.github.com>
2025-09-18 09:57:16 -0600

    Update ConsultorServiceImpl
    Implement consultant registration using the repository.

--- src/main/java/org/example/apilab2/service/implementation/ConsultorServiceImpl.java
+++ src/main/java/org/example/apilab2/service/implementation/ConsultorServiceImpl.java
@@ -1,4 +1,18 @@
 package org.example.apilab2.service.implementation;
 
-public class ConsultorServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.example.apilab2.repository.ConsultorRepository;
+import org.example.apilab2.repository.domain.Consultor;
+import org.example.apilab2.service.ConsultorService;
+import org.springframework.stereotype.Service;
+
+@Service @RequiredArgsConstructor
+public class ConsultorServiceImpl implements ConsultorService {
+    private final ConsultorRepository repo;
+
+    @Override
+    public Consultor crear(String nombre, String especializacion, Boolean inActivoActualizado) {
+        return repo.save(Consultor.builder()
+                .nombre(nombre).especializacion(especializacion).inActivoActualizado(inActivoActualizado).build());
+    }
 }
