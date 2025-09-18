b23e5899e2afc5944b18a63ad05e596f9cac882f
Carlos Eduardo Merino Ventura <102163019+Cyberking23@users.noreply.github.com>
2025-09-18 10:01:02 -0600

    Update ParticipanteService
    Implement participant registration.

--- src/main/java/org/example/apilab2/service/implementation/ParticipanteServiceImpl.java
+++ src/main/java/org/example/apilab2/service/implementation/ParticipanteServiceImpl.java
@@ -1,4 +1,17 @@
 package org.example.apilab2.service.implementation;
 
-public class ParticipanteServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.example.apilab2.repository.ParticipanteRepository;
+import org.example.apilab2.repository.domain.Participante;
+import org.example.apilab2.service.ParticipanteService;
+import org.springframework.stereotype.Service;
+
+@Service @RequiredArgsConstructor
+public class ParticipanteServiceImpl implements ParticipanteService {
+    private final ParticipanteRepository repo;
+    @Override
+    public Participante crear(String nombre, String email, String genero, String nivelEducativo, Boolean ingresoFormal) {
+        return repo.save(Participante.builder()
+                .nombre(nombre).email(email).genero(genero).nivelEducativo(nivelEducativo).ingresoFormal(ingresoFormal).build());
+    }
 }
